31f7f3549b5e82547942ecafa5b6e55db4ed47cd
Rugvip <poldsberg@gmail.com>
2025-01-23 15:14:46 +0000

    Deploying to gh-pages from @ backstage/backstage@2dc6c738e136117356cc053e5cb3453f75cf78e6 🚀

---
+++ storybook/assets/DefaultTemplate.stories-ChDNHvsy.js
@@ -0,0 +1,48 @@
+import{R as t}from"./index-CTjT7uj6.js";import{s as d,H as u}from"./plugin-EaS5ji_f.js";import{e as g}from"./routes-1XfYW0Rv.js";import{c as f}from"./api-D73ioj2B.js";import{s as h}from"./api-CKDuuaL7.js";import{C as y}from"./MockTranslationApi-7lcFbsnT.js";import{c as S}from"./catalogApiMock-CpzB4PJo.js";import{m as p}from"./makeStyles-Z1piKFaZ.js";import{S as E}from"./SearchContext-BQYqBmky.js";import{P as k}from"./Page-B52FpsWu.js";import{C}from"./Content-CBEMCsIV.js";import{S as r}from"./Grid-D0t_NOHX.js";import{b as x,a as P,c as T}from"./plugin-Bt1Rp8Kg.js";import{T as I}from"./TemplateBackstageLogo-mJYUyPb-.js";import{T as v}from"./TemplateBackstageLogoIcon-C_e9ZwiK.js";import{w as G}from"./appWrappers-CXaqpT8l.js";import{T as A}from"./TestApiProvider-CoiMLMhd.js";import{s as B}from"./StarredEntitiesApi-n7ClACGI.js";import{c as b}from"./ConfigApi-DBUUc3nU.js";import{M as R}from"./MockStarredEntitiesApi-CFn8c-CW.js";import{I as H}from"./InfoCard-B3VN9ET2.js";import"./iframe-D7sigK_m.js";import"../sb-preview/runtime.js";import"./index-Dj_lShYJ.js";import"./ApiRef-BSBwTmJJ.js";import"./Plugin-BnFuRug-.js";import"./componentData-CM4hSmEF.js";import"./useAnalytics-DTrkS1Gy.js";import"./index-DNAWfEOe.js";import"./useRouteRef-D0BCYqYb.js";import"./ref-8pKZtiZi.js";import"./index-CFaqwFgm.js";import"./defaultTheme-BrZMNV87.js";import"./classCallCheck-BNzALLS0.js";import"./inherits-_rA3tygc.js";import"./toArray-DrgApUvy.js";import"./TranslationApi-eOxINumg.js";import"./lodash-CoGan1YB.js";import"./useAsync-CXA3qup_.js";import"./useMountedState-DkESzBh4.js";import"./ThemeProvider-D-Ryobkw.js";import"./index-Cqve-NHl.js";import"./capitalize-CPKBZ2U-.js";import"./withStyles-BoERxSk9.js";import"./hoist-non-react-statics.cjs-DzIEFHQI.js";import"./interopRequireDefault-Y9pwbXtE.js";import"./createSvgIcon-CxejcRCW.js";import"./useControlled-CogIeAPD.js";import"./index-QA7F3UF1.js";import"./createSvgIcon-DvLPikTD.js";import"./isMuiElement-Cb6QZSLO.js";import"./unstable_useId-B3Hiq1YI.js";import"./IconButton-DMMb49NM.js";import"./ButtonBase-g64HK8if.js";import"./TransitionGroupContext-BtzQ-Cv7.js";import"./DialogTitle-ByShSbeU.js";import"./Modal-D7Vu5nWs.js";import"./Portal-DHXjgkAG.js";import"./Backdrop-DOPOOsuy.js";import"./useTheme-CxCkHA_V.js";import"./utils-BJxIKHMK.js";import"./Paper-C4Wu2ZRy.js";import"./Typography-DhqftZOJ.js";import"./Button-DOGLxW6B.js";import"./WebStorage-D5Fgivzj.js";import"./mapValues-D5qC5jJW.js";import"./toString-bNjqhuyi.js";import"./ApiProvider-CuNQiN7Z.js";import"./index-BRV0Se7Z.js";import"./CssBaseline-qxBnr2ND.js";import"./jsx-runtime-Cw0GR0a5.js";import"./palettes-B20oCNII.js";import"./CardActions-CvvgXdXY.js";import"./CardContent-DkDiQ52B.js";import"./ErrorBoundary-BD87Put7.js";import"./translation-67MxozS3.js";import"./ErrorPanel-CfpN4FfR.js";import"./WarningPanel-eKQsS6Xa.js";import"./ExpandMore-fUrsHI5d.js";import"./AccordionDetails-VxrhUSYf.js";import"./react-is.production.min-D0tnNtx9.js";import"./Collapse-DI41RIHM.js";import"./MarkdownContent-yj9pfUH1.js";import"./CodeSnippet-BAiYHTvR.js";import"./extends-CUPdUbmP.js";import"./toConsumableArray-BEwiObev.js";import"./Box-BuQ0oRjb.js";import"./typography-DfA0rp-6.js";import"./CopyTextButton-zFE9GXBx.js";import"./useCopyToClipboard-BW-rljyA.js";import"./Tooltip-dvcDS0rd.js";import"./Popper-B1XKQEe1.js";import"./Grow-GUSO4DqQ.js";import"./List-DOQrL9Id.js";import"./ListContext-DydK1sOh.js";import"./ListItem-Iyhdukvy.js";import"./ListItemText-B8eG4_-Y.js";import"./LinkButton-CEC0iDVK.js";import"./Link-CAwncZ2f.js";import"./CardHeader-rfX4dTX4.js";import"./Divider-N7UmmMDz.js";import"./BottomLink-DlHBrDqb.js";import"./ArrowForward-oSCFFhSc.js";const w=[{apiVersion:"1",kind:"Component",metadata:{name:"mock-starred-entity",title:"Mock Starred Entity!"}},{apiVersion:"1",kind:"Component",metadata:{name:"mock-starred-entity-2",title:"Mock Starred Entity 2!"}},{apiVersion:"1",kind:"Component",metadata:{name:"mock-starred-entity-3",title:"Mock Starred Entity 3!"}},{apiVersion:"1",kind:"Component",metadata:{name:"mock-starred-entity-4",title:"Mock Starred Entity 4!"}}],L=S({entities:w}),a=new R;a.toggleStarred("component:default/example-starred-entity");a.toggleStarred("component:default/example-starred-entity-2");a.toggleStarred("component:default/example-starred-entity-3");a.toggleStarred("component:default/example-starred-entity-4");const He={title:"Plugins/Home/Templates",decorators:[e=>G(t.createElement(t.Fragment,null,t.createElement(A,{apis:[[f,L],[B,a],[h,{query:()=>Promise.resolve({results:[]})}],[b,new y({stackoverflow:{baseUrl:"https://api.stackexchange.com/2.2"}})]]},t.createElement(e,null))),{mountedRoutes:{"/hello-company":d.routes.root,"/catalog/:namespace/:kind/:name":g}})]},M=p(e=>({searchBarInput:{maxWidth:"60vw",margin:"auto",backgroundColor:e.palette.background.paper,borderRadius:"50px",boxShadow:e.shadows[1]},searchBarOutline:{borderStyle:"none"}})),O=p(e=>({container:{margin:e.spacing(5,0)},svg:{width:"auto",height:100},path:{fill:"#7df3e1"}})),o=()=>{const e=M(),{svg:s,path:l,container:c}=O();return t.createElement(E,null,t.createElement(k,{themeId:"home"},t.createElement(C,null,t.createElement(r,{container:!0,justifyContent:"center",spacing:6},t.createElement(x,{className:c,logo:t.createElement(I,{classes:{svg:s,path:l}})}),t.createElement(r,{container:!0,item:!0,xs:12,justifyContent:"center"},t.createElement(u,{InputProps:{classes:{root:e.searchBarInput,notchedOutline:e.searchBarOutline}},placeholder:"Search"})),t.createElement(r,{container:!0,item:!0,xs:12},t.createElement(r,{item:!0,xs:12,md:6},t.createElement(P,null)),t.createElement(r,{item:!0,xs:12,md:6},t.createElement(T,{tools:Array(8).fill({url:"#",label:"link",icon:t.createElement(v,null)})})),t.createElement(r,{item:!0,xs:12,md:6},t.createElement(H,{title:"Composable Section"},t.createElement("div",{style:{height:370}}))))))))};o.__docgenInfo={description:"",methods:[],displayName:"DefaultTemplate"};var i,m,n;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`() => {
+  const classes = useStyles();
+  const {
+    svg,
+    path,
+    container
+  } = useLogoStyles();
+  return <SearchContextProvider>
+      <Page themeId="home">
+        <Content>
+          <Grid container justifyContent="center" spacing={6}>
+            <HomePageCompanyLogo className={container} logo={<TemplateBackstageLogo classes={{
+            svg,
+            path
+          }} />} />
+            <Grid container item xs={12} justifyContent="center">
+              <HomePageSearchBar InputProps={{
+              classes: {
+                root: classes.searchBarInput,
+                notchedOutline: classes.searchBarOutline
+              }
+            }} placeholder="Search" />
+            </Grid>
+            <Grid container item xs={12}>
+              <Grid item xs={12} md={6}>
+                <HomePageStarredEntities />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <HomePageToolkit tools={Array(8).fill({
+                url: '#',
+                label: 'link',
+                icon: <TemplateBackstageLogoIcon />
+              })} />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <InfoCard title="Composable Section">
+                  {/* placeholder for content */}
+                  <div style={{
+                  height: 370
+                }} />
+                </InfoCard>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Content>
+      </Page>
+    </SearchContextProvider>;
+}`,...(n=(m=o.parameters)==null?void 0:m.docs)==null?void 0:n.source}}};const we=["DefaultTemplate"];export{o as DefaultTemplate,we as __namedExportsOrder,He as default};

5197232f9e2c7932c478195db1ac86a4fc003373
Rugvip <poldsberg@gmail.com>
2025-02-12 20:40:25 +0000

    Deploying to gh-pages from @ backstage/backstage@9c3eb928efefc3d3e588390b5f432aef212ae0a3 🚀

---
+++ storybook/assets/SearchModal.stories-Dm0DFMmG.js
@@ -0,0 +1,69 @@
+import{d as M}from"./Close-XDuin5Qz.js";import{R as t}from"./index-CTjT7uj6.js";import{r as E}from"./plugin-Cvnj8XsT.js";import{S as s,u as h,a as C}from"./useSearchModal-Z2JCXvNL.js";import{s as y,M as x}from"./api-CKDuuaL7.js";import{S as I}from"./SearchContext-BQYqBmky.js";import{B as S}from"./Button-DddaZKrB.js";import{m as R}from"./makeStyles-3WuthtJ7.js";import{a as k,b as B,c as D}from"./DialogTitle-C1CUgfvO.js";import{B as T}from"./Box-BZcLdGyY.js";import{I as v}from"./IconButton-CKkyHIBQ.js";import{S as r}from"./Grid-Cd4CaOSn.js";import{S as b}from"./SearchType--d03W43T.js";import{L as G}from"./List-BslH4zsa.js";import{H as A}from"./DefaultResultListItem-ChQVXsLQ.js";import{w as N}from"./appWrappers-BKy5LqQt.js";import{T as _}from"./TestApiProvider-CoiMLMhd.js";import{SearchBar as P}from"./SearchBar-BtsVE4Pj.js";import{a as L}from"./SearchResult-C63c6dFO.js";import"./interopRequireDefault-Y9pwbXtE.js";import"./createSvgIcon-C4PsBp0A.js";import"./capitalize-CjHL08xv.js";import"./defaultTheme-U8IXQtr7.js";import"./withStyles-Dj_puyu8.js";import"./hoist-non-react-statics.cjs-DzIEFHQI.js";import"./useControlled-CogIeAPD.js";import"./index-QA7F3UF1.js";import"./createSvgIcon-CAWH9WqG.js";import"./isMuiElement-Cb6QZSLO.js";import"./unstable_useId-B3Hiq1YI.js";import"./iframe-BTyXU8_V.js";import"../sb-preview/runtime.js";import"./index-Dj_lShYJ.js";import"./ApiRef-BSBwTmJJ.js";import"./Plugin-CuVBUtJY.js";import"./componentData-CM4hSmEF.js";import"./useAnalytics-DTrkS1Gy.js";import"./ConfigApi-DBUUc3nU.js";import"./index-BkN7i-fW.js";import"./useRouteRef-ahYS9ypG.js";import"./ArrowForward-DOCYAoSf.js";import"./useTheme-hfNS2WFw.js";import"./Page-CBHregaw.js";import"./useMediaQuery-DtB-IR2v.js";import"./Divider-BhISGdDt.js";import"./ArrowBackIos-DirCxMW0.js";import"./ArrowForwardIos-B1FPkd7-.js";import"./lodash-CoGan1YB.js";import"./useAsync-CXA3qup_.js";import"./useMountedState-DkESzBh4.js";import"./ButtonBase-tHw6RLJZ.js";import"./TransitionGroupContext-BtzQ-Cv7.js";import"./Modal-BZrBnL3T.js";import"./classCallCheck-BNzALLS0.js";import"./Portal-DHXjgkAG.js";import"./Backdrop-By9NW1Qq.js";import"./utils-D7nA_lC_.js";import"./Paper-BZKq1osr.js";import"./Typography-CUBppVl0.js";import"./typography-hVTC7Hfk.js";import"./ExpandMore-DmzvNbMz.js";import"./AccordionDetails-eYVHbF9E.js";import"./toArray-QeYAVC82.js";import"./react-is.production.min-D0tnNtx9.js";import"./Collapse-B9Du_OuQ.js";import"./ListItem-C1ZFbryo.js";import"./ListContext-DydK1sOh.js";import"./ListItemIcon-YXvqEhg1.js";import"./ListItemText-CjIJj2RO.js";import"./Tabs-DNPpQI-u.js";import"./KeyboardArrowRight-B8I4OV05.js";import"./FormLabel-BN0n30g3.js";import"./formControlState-ByiNFc8I.js";import"./useFormControl-F4cJWIkJ.js";import"./InputLabel-B6CTjWLV.js";import"./Select-ChyfPUez.js";import"./Popover-EHwVSRZs.js";import"./Grow-BMsAcGvY.js";import"./Chip-DDsPU_OQ.js";import"./MenuItem-8NBQdGwV.js";import"./Checkbox-FMDCIR7x.js";import"./SwitchBase-Bh4HYQtf.js";import"./Link-Diak7Gn3.js";import"./index-Cqve-NHl.js";import"./MockTranslationApi-YWe2Dh37.js";import"./index-CFaqwFgm.js";import"./inherits-Cm41Z5uw.js";import"./TranslationApi-eOxINumg.js";import"./WebStorage-D5Fgivzj.js";import"./isSymbol-C_KZXW2d.js";import"./isObject-DlTwUI3n.js";import"./toString-B79bsZRM.js";import"./ApiProvider-CuNQiN7Z.js";import"./index-BRV0Se7Z.js";import"./CssBaseline-B78aEvSr.js";import"./ThemeProvider-HGIy1WSf.js";import"./jsx-runtime-Cw0GR0a5.js";import"./palettes-B20oCNII.js";import"./Search-BaTPO-V4.js";import"./useDebounce-4fqASXX_.js";import"./TextField-5MIvqfyc.js";import"./InputAdornment--MNz9KjP.js";import"./useElementFilter-KE6GOmb6.js";import"./Progress-CpJYT-_U.js";import"./LinearProgress-Cz5LTvAh.js";import"./ResponseErrorPanel-DysqszgC.js";import"./ErrorPanel-DFEQyeMA.js";import"./WarningPanel-D3799XoH.js";import"./MarkdownContent-CH4CBP6z.js";import"./CodeSnippet-Dr89H1XT.js";import"./objectWithoutProperties-Et-85jJS.js";import"./toConsumableArray-BEwiObev.js";import"./CopyTextButton-Bnk_mttM.js";import"./useCopyToClipboard-C1ehKsCH.js";import"./translation-67MxozS3.js";import"./Tooltip-n646yQ2-.js";import"./Popper-RF-Wjgln.js";import"./EmptyState-DspaHKHe.js";const w={results:[{type:"custom-result-item",document:{location:"search/search-result-1",title:"Search Result 1",text:"some text from the search result"}},{type:"no-custom-result-item",document:{location:"search/search-result-2",title:"Search Result 2",text:"some text from the search result"}},{type:"no-custom-result-item",document:{location:"search/search-result-3",title:"Search Result 3",text:"some text from the search result"}}]},Ue={title:"Plugins/Search/SearchModal",component:s,decorators:[e=>N(t.createElement(_,{apis:[[y,new x(w)]]},t.createElement(I,null,t.createElement(e,null))),{mountedRoutes:{"/search":E}})]},a=()=>{const{state:e,toggleModal:m}=h();return t.createElement(t.Fragment,null,t.createElement(S,{variant:"contained",color:"primary",onClick:m},"Toggle Search Modal"),t.createElement(s,{...e,toggleModal:m}))},F=R(e=>({titleContainer:{display:"flex",alignItems:"center",gap:e.spacing(1)},input:{flex:1},dialogActionsContainer:{padding:e.spacing(1,3)}})),i=()=>{const e=F(),{state:m,toggleModal:o}=h();return t.createElement(t.Fragment,null,t.createElement(S,{variant:"contained",color:"primary",onClick:o},"Toggle Custom Search Modal"),t.createElement(s,{...m,toggleModal:o},()=>t.createElement(t.Fragment,null,t.createElement(k,null,t.createElement(T,{className:e.titleContainer},t.createElement(P,{className:e.input}),t.createElement(v,{"aria-label":"close",onClick:o},t.createElement(M,null)))),t.createElement(B,null,t.createElement(r,{container:!0,direction:"column"},t.createElement(r,{item:!0},t.createElement(b.Tabs,{defaultValue:"",types:[{value:"custom-result-item",name:"Custom Item"},{value:"no-custom-result-item",name:"No Custom Item"}]})),t.createElement(r,{item:!0},t.createElement(L,null,({results:f})=>t.createElement(G,null,f.map(({document:l})=>t.createElement("div",{role:"button",tabIndex:0,key:`${l.location}-btn`,onClick:o,onKeyPress:o},t.createElement(A,{key:l.location,result:l})))))))),t.createElement(D,{className:e.dialogActionsContainer},t.createElement(r,{container:!0,direction:"row"},t.createElement(r,{item:!0,xs:12},t.createElement(C,null)))))))};a.__docgenInfo={description:"",methods:[],displayName:"Default"};i.__docgenInfo={description:"",methods:[],displayName:"CustomModal"};var n,c,p;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`() => {
+  const {
+    state,
+    toggleModal
+  } = useSearchModal();
+  return <>
+      <Button variant="contained" color="primary" onClick={toggleModal}>
+        Toggle Search Modal
+      </Button>
+      <SearchModal {...state} toggleModal={toggleModal} />
+    </>;
+}`,...(p=(c=a.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var u,d,g;i.parameters={...i.parameters,docs:{...(u=i.parameters)==null?void 0:u.docs,source:{originalSource:`() => {
+  const classes = useStyles();
+  const {
+    state,
+    toggleModal
+  } = useSearchModal();
+  return <>
+      <Button variant="contained" color="primary" onClick={toggleModal}>
+        Toggle Custom Search Modal
+      </Button>
+      <SearchModal {...state} toggleModal={toggleModal}>
+        {() => <>
+            <DialogTitle>
+              <Box className={classes.titleContainer}>
+                <SearchBar className={classes.input} />
+
+                <IconButton aria-label="close" onClick={toggleModal}>
+                  <CloseIcon />
+                </IconButton>
+              </Box>
+            </DialogTitle>
+            <DialogContent>
+              <Grid container direction="column">
+                <Grid item>
+                  <SearchType.Tabs defaultValue="" types={[{
+                value: 'custom-result-item',
+                name: 'Custom Item'
+              }, {
+                value: 'no-custom-result-item',
+                name: 'No Custom Item'
+              }]} />
+                </Grid>
+                <Grid item>
+                  <SearchResult>
+                    {({
+                  results
+                }) => <List>
+                        {results.map(({
+                    document
+                  }) => <div role="button" tabIndex={0} key={\`\${document.location}-btn\`} onClick={toggleModal} onKeyPress={toggleModal}>
+                            <DefaultResultListItem key={document.location} result={document} />
+                          </div>)}
+                      </List>}
+                  </SearchResult>
+                </Grid>
+              </Grid>
+            </DialogContent>
+            <DialogActions className={classes.dialogActionsContainer}>
+              <Grid container direction="row">
+                <Grid item xs={12}>
+                  <SearchResultPager />
+                </Grid>
+              </Grid>
+            </DialogActions>
+          </>}
+      </SearchModal>
+    </>;
+}`,...(g=(d=i.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};const We=["Default","CustomModal"];export{i as CustomModal,a as Default,We as __namedExportsOrder,Ue as default};

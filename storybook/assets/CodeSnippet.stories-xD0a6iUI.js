7423b4d9d3702fc6b3446864401afe91e32c662e
camilaibs <camilaibs@gmail.com>
2025-03-05 17:22:13 +0000

    Deploying to gh-pages from @ backstage/backstage@ebb68be1bdd294ce08726b5fc0ce0ffdab3cd9de 🚀

---
+++ storybook/assets/CodeSnippet.stories-xD0a6iUI.js
@@ -0,0 +1,39 @@
+import{R as e}from"./index-CTjT7uj6.js";import{C as t}from"./CodeSnippet-Dj6Lpx2o.js";import{I as o}from"./InfoCard-BhBRkRK-.js";import"./iframe-C8RthN9k.js";import"../sb-preview/runtime.js";import"./defaultTheme-DquFOgf8.js";import"./extends-CUPdUbmP.js";import"./classCallCheck-BNzALLS0.js";import"./inherits-BBh9Yz5k.js";import"./toConsumableArray-BEwiObev.js";import"./useTheme-0ztDbzjM.js";import"./Box-DU2WS7ls.js";import"./typography-1q_m8518.js";import"./hoist-non-react-statics.cjs-DzIEFHQI.js";import"./CopyTextButton-DYapF7Y1.js";import"./useCopyToClipboard-D4ko06Zm.js";import"./interopRequireDefault-Y9pwbXtE.js";import"./createSvgIcon-D7HBgcTi.js";import"./capitalize-BWjKmKKm.js";import"./withStyles-DWaS6n8x.js";import"./useControlled-Hu_S6sGp.js";import"./createSvgIcon-CL6P1I3F.js";import"./debounce-BO-l2B6S.js";import"./isMuiElement-B_4ddUuK.js";import"./useIsFocusVisible-Cc1qTAFu.js";import"./index-QA7F3UF1.js";import"./unstable_useId-B3Hiq1YI.js";import"./useMountedState-DkESzBh4.js";import"./translation-67MxozS3.js";import"./TranslationApi-eOxINumg.js";import"./ApiRef-BSBwTmJJ.js";import"./Tooltip-CEAy7RgQ.js";import"./Popper-OF5Y-VYl.js";import"./Portal-C-K0NGyB.js";import"./Grow-IFUUo7Bg.js";import"./utils-C77xYEoA.js";import"./TransitionGroupContext-BtzQ-Cv7.js";import"./IconButton-N_BMqCoX.js";import"./ButtonBase-C5vBVldS.js";import"./index-Cqve-NHl.js";import"./makeStyles-CRB_T0k9.js";import"./CardActions-COEhqCRe.js";import"./CardContent-OuGBBiNv.js";import"./Paper-2nKWzoda.js";import"./ErrorBoundary-DP9UkD-x.js";import"./ErrorPanel-BudUEpOC.js";import"./WarningPanel-DFhatmgU.js";import"./ExpandMore-BFxc8y0x.js";import"./AccordionDetails-CbECkQ3e.js";import"./toArray-n7tUSQe9.js";import"./react-is.production.min-D0tnNtx9.js";import"./Collapse-r4tqKWw-.js";import"./Typography-D5Gm01bp.js";import"./MarkdownContent-sS5RIzEz.js";import"./index-BRV0Se7Z.js";import"./Grid-CFE-uVXf.js";import"./List-B21WyO9K.js";import"./ListContext-DydK1sOh.js";import"./ListItem-BqNetBt0.js";import"./ListItemText-BQG6-PBZ.js";import"./LinkButton--_bXbXhy.js";import"./Link-w7sdsOKM.js";import"./lodash-CoGan1YB.js";import"./index-BkN7i-fW.js";import"./useAnalytics-DTrkS1Gy.js";import"./ConfigApi-DBUUc3nU.js";import"./Button-DIqXZjyu.js";import"./CardHeader-CsO1doVL.js";import"./Divider-fLtrH1nN.js";import"./BottomLink-CE-BH3Aq.js";import"./ArrowForward-BXpTofhf.js";const Ke={title:"Data Display/CodeSnippet",component:t},l={width:300},r=`const greeting = "Hello";
+const world = "World";
+
+const greet = person => greeting + " " + person + "!";
+
+greet(world);
+`,j=`const greeting: string = "Hello";
+const world: string = "World";
+
+const greet = (person: string): string => greeting + " " + person + "!";
+
+greet(world);
+`,A=`greeting = "Hello"
+world = "World"
+
+def greet(person):
+    return f"{greeting} {person}!"
+
+greet(world)
+`,a=()=>e.createElement(o,{title:"JavaScript example"},e.createElement(t,{text:"const hello = 'World';",language:"javascript"})),i=()=>e.createElement(o,{title:"JavaScript multi-line example"},e.createElement(t,{text:r,language:"javascript"})),p=()=>e.createElement(o,{title:"Show line numbers"},e.createElement(t,{text:r,language:"javascript",showLineNumbers:!0})),n=()=>e.createElement(o,{title:"Overflow"},e.createElement("div",{style:l},e.createElement(t,{text:r,language:"javascript"})),e.createElement("div",{style:l},e.createElement(t,{text:r,language:"javascript",showLineNumbers:!0}))),s=()=>e.createElement(o,{title:"Multiple languages"},e.createElement(t,{text:r,language:"javascript",showLineNumbers:!0}),e.createElement(t,{text:j,language:"typescript",showLineNumbers:!0}),e.createElement(t,{text:A,language:"python",showLineNumbers:!0})),m=()=>e.createElement(o,{title:"Copy Code"},e.createElement(t,{text:r,language:"javascript",showCopyCodeButton:!0}));a.__docgenInfo={description:"",methods:[],displayName:"Default"};i.__docgenInfo={description:"",methods:[],displayName:"MultipleLines"};p.__docgenInfo={description:"",methods:[],displayName:"LineNumbers"};n.__docgenInfo={description:"",methods:[],displayName:"Overflow"};s.__docgenInfo={description:"",methods:[],displayName:"Languages"};m.__docgenInfo={description:"",methods:[],displayName:"CopyCode"};var c,d,g;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`() => <InfoCard title="JavaScript example">
+    <CodeSnippet text="const hello = 'World';" language="javascript" />
+  </InfoCard>`,...(g=(d=a.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var u,C,S;i.parameters={...i.parameters,docs:{...(u=i.parameters)==null?void 0:u.docs,source:{originalSource:`() => <InfoCard title="JavaScript multi-line example">
+    <CodeSnippet text={JAVASCRIPT} language="javascript" />
+  </InfoCard>`,...(S=(C=i.parameters)==null?void 0:C.docs)==null?void 0:S.source}}};var f,v,I;p.parameters={...p.parameters,docs:{...(f=p.parameters)==null?void 0:f.docs,source:{originalSource:`() => <InfoCard title="Show line numbers">
+    <CodeSnippet text={JAVASCRIPT} language="javascript" showLineNumbers />
+  </InfoCard>`,...(I=(v=p.parameters)==null?void 0:v.docs)==null?void 0:I.source}}};var w,h,y;n.parameters={...n.parameters,docs:{...(w=n.parameters)==null?void 0:w.docs,source:{originalSource:`() => <InfoCard title="Overflow">
+    <div style={containerStyle}>
+      <CodeSnippet text={JAVASCRIPT} language="javascript" />
+    </div>
+    <div style={containerStyle}>
+      <CodeSnippet text={JAVASCRIPT} language="javascript" showLineNumbers />
+    </div>
+  </InfoCard>`,...(y=(h=n.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};var x,N,E;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`() => <InfoCard title="Multiple languages">
+    <CodeSnippet text={JAVASCRIPT} language="javascript" showLineNumbers />
+    <CodeSnippet text={TYPESCRIPT} language="typescript" showLineNumbers />
+    <CodeSnippet text={PYTHON} language="python" showLineNumbers />
+  </InfoCard>`,...(E=(N=s.parameters)==null?void 0:N.docs)==null?void 0:E.source}}};var L,b,_;m.parameters={...m.parameters,docs:{...(L=m.parameters)==null?void 0:L.docs,source:{originalSource:`() => <InfoCard title="Copy Code">
+    <CodeSnippet text={JAVASCRIPT} language="javascript" showCopyCodeButton />
+  </InfoCard>`,...(_=(b=m.parameters)==null?void 0:b.docs)==null?void 0:_.source}}};const Qe=["Default","MultipleLines","LineNumbers","Overflow","Languages","CopyCode"];export{m as CopyCode,a as Default,s as Languages,p as LineNumbers,i as MultipleLines,n as Overflow,Qe as __namedExportsOrder,Ke as default};

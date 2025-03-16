d4d3c97fe2e98889c4b3968dfbecb954f7ba5b8e
freben <freben@gmail.com>
2025-03-16 16:42:54 +0000

    Deploying to gh-pages from @ backstage/backstage@d81cdcc45d2ed106c3cf69a12a6d0c15d9df4720 🚀

---
+++ storybook/assets/SearchResultList.stories-CisHqujf.js
@@ -0,0 +1,75 @@
+import{R as e,r as s}from"./index-CTjT7uj6.js";import{s as h,M as S}from"./api-DbMoNnVF.js";import{c as J}from"./SearchResult-BnNcsW21.js";import{S as o}from"./SearchResultList-D7PSBK1D.js";import{S as R}from"./Grid-CFE-uVXf.js";import{S as K}from"./SearchContext-CNAQSFXU.js";import{L as j}from"./ListItemText-BQG6-PBZ.js";import{H as z}from"./DefaultResultListItem-B2P-d58V.js";import{C as U}from"./icons-BpKbDtVo.js";import{L as V}from"./ListItem-CkBp7laK.js";import{L as X}from"./ListItemIcon-CM5i1NWL.js";import{w as Y,a as Z}from"./appWrappers-MUaV3VUY.js";import{T as y}from"./TestApiProvider-CmAAIwv0.js";import{L as $}from"./Link-CRcM_l-0.js";import{c as ee}from"./Plugin-B9fYbQX8.js";import"./ApiRef-DUoQ5WvO.js";import"./useAsync-CXA3qup_.js";import"./useMountedState-DkESzBh4.js";import"./lodash-CoGan1YB.js";import"./List-B21WyO9K.js";import"./defaultTheme-DquFOgf8.js";import"./capitalize-BWjKmKKm.js";import"./withStyles-DWaS6n8x.js";import"./hoist-non-react-statics.cjs-DzIEFHQI.js";import"./ListContext-DydK1sOh.js";import"./useElementFilter-CZ7Q4DZU.js";import"./componentData-BzTcs2ef.js";import"./MockTranslationApi-D0CDTXL6.js";import"./index-CFaqwFgm.js";import"./classCallCheck-BNzALLS0.js";import"./inherits-BBh9Yz5k.js";import"./toArray-n7tUSQe9.js";import"./TranslationApi-TmuXcaNh.js";import"./useAnalytics-Dmj0Ntiu.js";import"./ConfigApi-BZwRYjFH.js";import"./Progress-B9NvVbef.js";import"./useTheme-0ztDbzjM.js";import"./LinearProgress-mbGV-ZMg.js";import"./Box-DU2WS7ls.js";import"./typography-1q_m8518.js";import"./ResponseErrorPanel-BLWhX3hb.js";import"./makeStyles-CRB_T0k9.js";import"./ErrorPanel-99lIuJhN.js";import"./WarningPanel-CT0EEl71.js";import"./interopRequireDefault-Y9pwbXtE.js";import"./createSvgIcon-4utR8_H1.js";import"./createChainedFunction-Da-WpsAN.js";import"./createSvgIcon-CL6P1I3F.js";import"./debounce-DtXjJkxj.js";import"./isMuiElement-B_4ddUuK.js";import"./ownerWindow-C3iVrxHF.js";import"./useIsFocusVisible-DdvWPq7A.js";import"./index-BhgyLgKK.js";import"./useControlled-B47E2WMp.js";import"./unstable_useId-B3Hiq1YI.js";import"./ExpandMore-Bld4j_rr.js";import"./AccordionDetails-f-MlAr3f.js";import"./react-is.production.min-D0tnNtx9.js";import"./Paper-2nKWzoda.js";import"./Collapse-BeLJ-1Ro.js";import"./utils-h0SkNxCQ.js";import"./TransitionGroupContext-BtzQ-Cv7.js";import"./ButtonBase-nYwe-awX.js";import"./IconButton-8PCPDGIr.js";import"./Typography-D5Gm01bp.js";import"./MarkdownContent-BjXvx0Tw.js";import"./index-BRV0Se7Z.js";import"./CodeSnippet-BRv5GUQh.js";import"./iframe-BaySzl5A.js";import"./extends-CUPdUbmP.js";import"./toConsumableArray-BEwiObev.js";import"./CopyTextButton-DuMIoCnX.js";import"./useCopyToClipboard-PM_f3mLj.js";import"./translation-b4xNyudL.js";import"./Tooltip-3svt0R8o.js";import"./Popper-D9fa09d_.js";import"./Portal-BLjQd1nq.js";import"./Grow-BqP4q7YH.js";import"./Divider-fLtrH1nN.js";import"./EmptyState-BboKgqaL.js";import"./index-CNMBxS8v.js";import"./WebStorage-D9doizkM.js";import"./isSymbol-C_KZXW2d.js";import"./isObject-DlTwUI3n.js";import"./toString-B79bsZRM.js";import"./ApiProvider-D8NVuZ3M.js";import"./CssBaseline-0XN_ELQr.js";import"./ThemeProvider-iV7LyTO2.js";import"./jsx-runtime-Cw0GR0a5.js";import"./palettes-C1DVN5xY.js";import"./index-Cqve-NHl.js";import"./useRouteRef-CVI83Ox1.js";const te=Z({id:"storybook.search.results.list.route"}),re=new S({results:[{type:"techdocs",document:{location:"search/search-result1",title:"Search Result 1",text:"Some text from the search result 1"}},{type:"custom",document:{location:"search/search-result2",title:"Search Result 2",text:"Some text from the search result 2"}}]}),jt={title:"Plugins/Search/SearchResultList",component:o,decorators:[t=>Y(e.createElement(y,{apis:[[h,re]]},e.createElement(R,{container:!0,direction:"row"},e.createElement(R,{item:!0,xs:12},e.createElement(t,null)))),{mountedRoutes:{"/":te}})]},n=()=>e.createElement(K,null,e.createElement(o,null)),a=()=>{const[t]=s.useState({types:["techdocs"]});return e.createElement(o,{query:t})},m=()=>{const[t]=s.useState({types:["techdocs"]});return e.createElement(y,{apis:[[h,{query:()=>new Promise(()=>{})}]]},e.createElement(o,{query:t}))},c=()=>{const[t]=s.useState({types:["techdocs"]});return e.createElement(y,{apis:[[h,{query:()=>new Promise(()=>{throw new Error})}]]},e.createElement(o,{query:t}))},p=()=>{const[t]=s.useState({types:["techdocs"]});return e.createElement(y,{apis:[[h,new S]]},e.createElement(o,{query:t}))},u=()=>{const[t]=s.useState({types:["techdocs"]});return e.createElement(y,{apis:[[h,new S]]},e.createElement(o,{query:t,noResultsComponent:e.createElement(j,{primary:"No results were found"})}))},oe=t=>{const{icon:i,result:r}=t;return e.createElement($,{to:r.location},e.createElement(V,{alignItems:"flex-start",divider:!0},i&&e.createElement(X,null,i),e.createElement(j,{primary:r.title,primaryTypographyProps:{variant:"h6"},secondary:r.text})))},l=()=>{const[t]=s.useState({types:["custom"]});return e.createElement(o,{query:t,renderResultItem:({type:i,document:r,highlight:B,rank:F})=>{switch(i){case"custom":return e.createElement(oe,{key:r.location,icon:e.createElement(U,null),result:r,highlight:B,rank:F});default:return e.createElement(z,{key:r.location,result:r})}}})},d=()=>{const[t]=s.useState({types:["techdocs"]}),r=ee({id:"plugin"}).provide(J({name:"DefaultResultListItem",component:async()=>z}));return e.createElement(o,{query:t},e.createElement(r,null))};n.__docgenInfo={description:"",methods:[],displayName:"Default"};a.__docgenInfo={description:"",methods:[],displayName:"WithQuery"};m.__docgenInfo={description:"",methods:[],displayName:"Loading"};c.__docgenInfo={description:"",methods:[],displayName:"WithError"};p.__docgenInfo={description:"",methods:[],displayName:"WithDefaultNoResultsComponent"};u.__docgenInfo={description:"",methods:[],displayName:"WithCustomNoResultsComponent"};l.__docgenInfo={description:"",methods:[],displayName:"WithCustomResultItem"};d.__docgenInfo={description:"",methods:[],displayName:"WithResultItemExtensions"};var f,g,E;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`() => {
+  return <SearchContextProvider>
+      <SearchResultList />
+    </SearchContextProvider>;
+}`,...(E=(g=n.parameters)==null?void 0:g.docs)==null?void 0:E.source}}};var I,L,q;a.parameters={...a.parameters,docs:{...(I=a.parameters)==null?void 0:I.docs,source:{originalSource:`() => {
+  const [query] = useState<Partial<SearchQuery>>({
+    types: ['techdocs']
+  });
+  return <SearchResultList query={query} />;
+}`,...(q=(L=a.parameters)==null?void 0:L.docs)==null?void 0:q.source}}};var P,C,w;m.parameters={...m.parameters,docs:{...(P=m.parameters)==null?void 0:P.docs,source:{originalSource:`() => {
+  const [query] = useState<Partial<SearchQuery>>({
+    types: ['techdocs']
+  });
+  return <TestApiProvider apis={[[searchApiRef, {
+    query: () => new Promise<SearchResultSet>(() => {})
+  }]]}>
+      <SearchResultList query={query} />
+    </TestApiProvider>;
+}`,...(w=(C=m.parameters)==null?void 0:C.docs)==null?void 0:w.source}}};var x,A,_;c.parameters={...c.parameters,docs:{...(x=c.parameters)==null?void 0:x.docs,source:{originalSource:`() => {
+  const [query] = useState<Partial<SearchQuery>>({
+    types: ['techdocs']
+  });
+  return <TestApiProvider apis={[[searchApiRef, {
+    query: () => new Promise<SearchResultSet>(() => {
+      throw new Error();
+    })
+  }]]}>
+      <SearchResultList query={query} />
+    </TestApiProvider>;
+}`,...(_=(A=c.parameters)==null?void 0:A.docs)==null?void 0:_.source}}};var W,v,N;p.parameters={...p.parameters,docs:{...(W=p.parameters)==null?void 0:W.docs,source:{originalSource:`() => {
+  const [query] = useState<Partial<SearchQuery>>({
+    types: ['techdocs']
+  });
+  return <TestApiProvider apis={[[searchApiRef, new MockSearchApi()]]}>
+      <SearchResultList query={query} />
+    </TestApiProvider>;
+}`,...(N=(v=p.parameters)==null?void 0:v.docs)==null?void 0:N.source}}};var D,T,k;u.parameters={...u.parameters,docs:{...(D=u.parameters)==null?void 0:D.docs,source:{originalSource:`() => {
+  const [query] = useState<Partial<SearchQuery>>({
+    types: ['techdocs']
+  });
+  return <TestApiProvider apis={[[searchApiRef, new MockSearchApi()]]}>
+      <SearchResultList query={query} noResultsComponent={<ListItemText primary="No results were found" />} />
+    </TestApiProvider>;
+}`,...(k=(T=u.parameters)==null?void 0:T.docs)==null?void 0:k.source}}};var Q,M,H;l.parameters={...l.parameters,docs:{...(Q=l.parameters)==null?void 0:Q.docs,source:{originalSource:`() => {
+  const [query] = useState<Partial<SearchQuery>>({
+    types: ['custom']
+  });
+  return <SearchResultList query={query} renderResultItem={({
+    type,
+    document,
+    highlight,
+    rank
+  }) => {
+    switch (type) {
+      case 'custom':
+        return <CustomResultListItem key={document.location} icon={<CatalogIcon />} result={document} highlight={highlight} rank={rank} />;
+      default:
+        return <DefaultResultListItem key={document.location} result={document} />;
+    }
+  }} />;
+}`,...(H=(M=l.parameters)==null?void 0:M.docs)==null?void 0:H.source}}};var O,b,G;d.parameters={...d.parameters,docs:{...(O=d.parameters)==null?void 0:O.docs,source:{originalSource:`() => {
+  const [query] = useState<Partial<SearchQuery>>({
+    types: ['techdocs']
+  });
+  const plugin = createPlugin({
+    id: 'plugin'
+  });
+  const DefaultSearchResultListItem = plugin.provide(createSearchResultListItemExtension({
+    name: 'DefaultResultListItem',
+    component: async () => DefaultResultListItem
+  }));
+  return <SearchResultList query={query}>
+      <DefaultSearchResultListItem />
+    </SearchResultList>;
+}`,...(G=(b=d.parameters)==null?void 0:b.docs)==null?void 0:G.source}}};const zt=["Default","WithQuery","Loading","WithError","WithDefaultNoResultsComponent","WithCustomNoResultsComponent","WithCustomResultItem","WithResultItemExtensions"];export{n as Default,m as Loading,u as WithCustomNoResultsComponent,l as WithCustomResultItem,p as WithDefaultNoResultsComponent,c as WithError,a as WithQuery,d as WithResultItemExtensions,zt as __namedExportsOrder,jt as default};

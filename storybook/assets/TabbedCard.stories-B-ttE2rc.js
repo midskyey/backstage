38fe1c02e6227ca570aa881ac81a4ca86d9b3011
acierto <bogdannechiporenko@gmail.com>
2025-03-18 09:24:31 +0000

    Deploying to gh-pages from @ backstage/backstage@a90075af6605d452abce6561989a88784da9a942 🚀

---
+++ storybook/assets/TabbedCard.stories-B-ttE2rc.js
@@ -0,0 +1,71 @@
+import{r as _,R as e}from"./index-CV65Cz2T.js";import{m as q}from"./makeStyles-B2ffWluB.js";import{w as M}from"./capitalize-BKQXumqD.js";import{C as P}from"./CardHeader-DAExE-wZ.js";import{C as D,a as V}from"./CardContent-B5iYBdc5.js";import{E as N}from"./ErrorBoundary-BXVjW1bX.js";import{T as A,a as F}from"./Tabs-DviUIT5-.js";import{D as j}from"./Divider-YBHdkOJo.js";import{B as G}from"./BottomLink-CQ5BwGoW.js";import{S as E}from"./Grid-BB-GYNIy.js";import{T as U}from"./Typography-ClGfHewG.js";import{M as X}from"./index-BuiljCIz.js";import"./defaultTheme-dorO8Ycy.js";import"./withStyles-5L4Wrt0H.js";import"./hoist-non-react-statics.cjs-DscmM33_.js";import"./Paper-BZMBdJdO.js";import"./translation-CnlFI5r0.js";import"./TranslationApi-BZ5LsqDR.js";import"./ApiRef-DSSgzaW4.js";import"./ErrorPanel-CtwI3OOl.js";import"./WarningPanel-CS4JIXKB.js";import"./interopRequireDefault-Y9pwbXtE.js";import"./createSvgIcon-esOf8MXJ.js";import"./createChainedFunction-Da-WpsAN.js";import"./createSvgIcon-BE9FbSc8.js";import"./debounce-DtXjJkxj.js";import"./isMuiElement-DG475NJP.js";import"./ownerWindow-D1FXuRdm.js";import"./useIsFocusVisible-DMiTmV9E.js";import"./index-CpD63NMF.js";import"./useControlled-ltvK4nzt.js";import"./unstable_useId-DsWtIzNz.js";import"./ExpandMore-CGIqGUc-.js";import"./AccordionDetails-Pkymkb_v.js";import"./toArray-COxZ3wB-.js";import"./react-is.production.min-D0tnNtx9.js";import"./Collapse-CydqjKxS.js";import"./utils-CaJHH84X.js";import"./TransitionGroupContext-C1n1zRT4.js";import"./useTheme-Dmunefpm.js";import"./ButtonBase-Ba4BxsIu.js";import"./IconButton-CiUvO6Eb.js";import"./MarkdownContent-C0_8Uxj-.js";import"./index-BDlCPcVq.js";import"./CodeSnippet-D0EJogpP.js";import"./iframe-Be_b3fxo.js";import"./objectWithoutProperties-Et-85jJS.js";import"./classCallCheck-BNzALLS0.js";import"./inherits-CeNoR6ST.js";import"./toConsumableArray-BEwiObev.js";import"./Box-D_ilMR00.js";import"./typography-Cs1aOEfH.js";import"./CopyTextButton-CWv1yKFy.js";import"./useCopyToClipboard-CXmmh4yb.js";import"./useMountedState-BG_s6PIB.js";import"./Tooltip-Dky-G2Xk.js";import"./Popper-CtVHcIVU.js";import"./Portal-Cz7V7drg.js";import"./Grow-JHyMhSnP.js";import"./List-D3T44CW6.js";import"./ListContext-DDd8B5Hf.js";import"./ListItem--gnxU6mZ.js";import"./ListItemText-Db7RxVPo.js";import"./LinkButton-PUAgVPlG.js";import"./Link-DPwPNjg6.js";import"./index-DSEhRz7_.js";import"./lodash-D2SO6J9k.js";import"./useAnalytics-BvU8z2Qw.js";import"./ConfigApi-BmcvyZGI.js";import"./Button-Bud8S-Ub.js";import"./KeyboardArrowRight-DUUcGvVE.js";import"./ArrowForward-IZFOs--K.js";const Y=q(t=>({root:{padding:t.spacing(0,2,0,2.5),minHeight:t.spacing(3)},indicator:{backgroundColor:t.palette.info.main,height:t.spacing(.3)}}),{name:"BackstageTabbedCard"}),Z=M(t=>({root:{padding:t.spacing(2,2,2,2.5),display:"inline-block"},title:{fontWeight:700},subheader:{paddingTop:t.spacing(1)}}),{name:"BackstageTabbedCardBoldHeader"})(P);function d(t){const{slackChannel:i,errorBoundaryProps:l,children:o,title:c,deepLink:y,value:u,onChange:v}=t,x=Y(),[T,H]=_.useState(0),I=v||((n,C)=>H(C));let b;u?e.Children.map(o,n=>{e.isValidElement(n)&&(n==null?void 0:n.props.value)===u&&(b=n==null?void 0:n.props.children)}):e.Children.map(o,(n,C)=>{e.isValidElement(n)&&C===T&&(b=n==null?void 0:n.props.children)});const B=l||(i?{slackChannel:i}:{});return e.createElement(D,null,e.createElement(N,{...B},c&&e.createElement(Z,{title:c}),e.createElement(A,{classes:x,value:u||T,onChange:I},o),e.createElement(j,null),e.createElement(V,null,b),y&&e.createElement(G,{...y})))}const z=q(t=>({root:{minWidth:t.spacing(6),minHeight:t.spacing(3),margin:t.spacing(0,2,0,0),padding:t.spacing(.5,0,.5,0),textTransform:"none","&:hover":{opacity:1,backgroundColor:"transparent",color:t.palette.text.primary}},selected:{fontWeight:t.typography.fontWeightBold}}),{name:"BackstageCardTab"});function r(t){const{children:i,...l}=t,o=z();return e.createElement(F,{disableRipple:!0,classes:o,...l})}d.__docgenInfo={description:"",methods:[],displayName:"TabbedCard",props:{slackChannel:{required:!1,tsType:{name:"string"},description:"@deprecated Use errorBoundaryProps instead"},errorBoundaryProps:{required:!1,tsType:{name:"ReactPropsWithChildren",raw:`React.PropsWithChildren<{
+  slackChannel?: string | SlackChannel;
+  onError?: (error: Error, errorInfo: string) => null;
+}>`,elements:[{name:"signature",type:"object",raw:`{
+  slackChannel?: string | SlackChannel;
+  onError?: (error: Error, errorInfo: string) => null;
+}`,signature:{properties:[{key:"slackChannel",value:{name:"union",raw:"string | SlackChannel",elements:[{name:"string"},{name:"signature",type:"object",raw:`{
+  name: string;
+  href?: string;
+}`,signature:{properties:[{key:"name",value:{name:"string",required:!0}},{key:"href",value:{name:"string",required:!1}}]}}],required:!1}},{key:"onError",value:{name:"signature",type:"function",raw:"(error: Error, errorInfo: string) => null",signature:{arguments:[{type:{name:"Error"},name:"error"},{type:{name:"string"},name:"errorInfo"}],return:{name:"null"}},required:!1}}]}}]},description:""},children:{required:!1,tsType:{name:"Array",elements:[{name:"ReactElement",elements:[{name:"TabProps"}],raw:"ReactElement<TabProps>"}],raw:"ReactElement<TabProps>[]"},description:""},onChange:{required:!1,tsType:{name:"signature",type:"function",raw:"(event: React.ChangeEvent<{}>, value: number | string) => void",signature:{arguments:[{type:{name:"ReactChangeEvent",raw:"React.ChangeEvent<{}>",elements:[{name:"signature",type:"object",raw:"{}",signature:{properties:[]}}]},name:"event"},{type:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}]},name:"value"}],return:{name:"void"}}},description:""},title:{required:!1,tsType:{name:"string"},description:""},value:{required:!1,tsType:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}]},description:""},deepLink:{required:!1,tsType:{name:"signature",type:"object",raw:`{
+  link: string;
+  title: string;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+}`,signature:{properties:[{key:"link",value:{name:"string",required:!0}},{key:"title",value:{name:"string",required:!0}},{key:"onClick",value:{name:"signature",type:"function",raw:"(event: React.MouseEvent<HTMLAnchorElement>) => void",signature:{arguments:[{type:{name:"ReactMouseEvent",raw:"React.MouseEvent<HTMLAnchorElement>",elements:[{name:"HTMLAnchorElement"}]},name:"event"}],return:{name:"void"}},required:!1}}]}},description:""}}};r.__docgenInfo={description:`Card tab component used in {@link TabbedCard}
+
+@public`,methods:[],displayName:"CardTab",props:{children:{required:!0,tsType:{name:"ReactNode"},description:""}}};const a={height:200,width:500},g=({children:t})=>e.createElement(X,null,t),Ct={title:"Layout/Tabbed Card",component:d,decorators:[t=>e.createElement(E,{container:!0,spacing:4},e.createElement(E,{item:!0},t()))]},s=()=>e.createElement(g,null,e.createElement(d,{title:"Default Example Header"},e.createElement(r,{label:"Option 1"},e.createElement("div",{style:a},"Some content")),e.createElement(r,{label:"Option 2"},e.createElement("div",{style:a},"Some content 2")),e.createElement(r,{label:"Option 3"},e.createElement("div",{style:a},"Some content 3")),e.createElement(r,{label:"Option 4"},e.createElement("div",{style:a},"Some content 4")))),J={title:"Go to XYZ Location",link:"#"},m=()=>e.createElement(g,null,e.createElement(d,{title:"Footer Link Example Header",deepLink:J},e.createElement(r,{label:"Option 1"},e.createElement("div",{style:a},"Some content")),e.createElement(r,{label:"Option 2"},e.createElement("div",{style:a},"Some content 2")),e.createElement(r,{label:"Option 3"},e.createElement("div",{style:a},"Some content 3")),e.createElement(r,{label:"Option 4"},e.createElement("div",{style:a},"Some content 4")))),p=()=>{const[t,i]=_.useState("one"),l=(o,c)=>i(c);return e.createElement(g,null,e.createElement(U,{component:"span"},"Selected tab is ",t),e.createElement(d,{value:t,onChange:l,title:"Controlled Value Example"},e.createElement(r,{value:"one",label:"Option 1"},e.createElement("div",{style:a},"Some content")),e.createElement(r,{value:"two",label:"Option 2"},e.createElement("div",{style:a},"Some content 2")),e.createElement(r,{value:"three",label:"Option 3"},e.createElement("div",{style:a},"Some content 3")),e.createElement(r,{value:"four",label:"Option 4"},e.createElement("div",{style:a},"Some content 4"))))};s.__docgenInfo={description:"",methods:[],displayName:"Default"};m.__docgenInfo={description:"",methods:[],displayName:"WithFooterLink"};p.__docgenInfo={description:"",methods:[],displayName:"WithControlledTabValue"};var S,f,h;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`() => {
+  return <Wrapper>
+      <TabbedCard title="Default Example Header">
+        <CardTab label="Option 1">
+          <div style={cardContentStyle}>Some content</div>
+        </CardTab>
+        <CardTab label="Option 2">
+          <div style={cardContentStyle}>Some content 2</div>
+        </CardTab>
+        <CardTab label="Option 3">
+          <div style={cardContentStyle}>Some content 3</div>
+        </CardTab>
+        <CardTab label="Option 4">
+          <div style={cardContentStyle}>Some content 4</div>
+        </CardTab>
+      </TabbedCard>
+    </Wrapper>;
+}`,...(h=(f=s.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var k,O,w;m.parameters={...m.parameters,docs:{...(k=m.parameters)==null?void 0:k.docs,source:{originalSource:`() => {
+  return <Wrapper>
+      <TabbedCard title="Footer Link Example Header" deepLink={linkInfo}>
+        <CardTab label="Option 1">
+          <div style={cardContentStyle}>Some content</div>
+        </CardTab>
+        <CardTab label="Option 2">
+          <div style={cardContentStyle}>Some content 2</div>
+        </CardTab>
+        <CardTab label="Option 3">
+          <div style={cardContentStyle}>Some content 3</div>
+        </CardTab>
+        <CardTab label="Option 4">
+          <div style={cardContentStyle}>Some content 4</div>
+        </CardTab>
+      </TabbedCard>
+    </Wrapper>;
+}`,...(w=(O=m.parameters)==null?void 0:O.docs)==null?void 0:w.source}}};var W,R,L;p.parameters={...p.parameters,docs:{...(W=p.parameters)==null?void 0:W.docs,source:{originalSource:`() => {
+  const [selectedTab, setSelectedTab] = useState<string | number>('one');
+  const handleChange = (_ev: any, newSelectedTab: string | number) => setSelectedTab(newSelectedTab);
+  return <Wrapper>
+      <Typography component="span">Selected tab is {selectedTab}</Typography>
+
+      <TabbedCard value={selectedTab} onChange={handleChange} title="Controlled Value Example">
+        <CardTab value="one" label="Option 1">
+          <div style={cardContentStyle}>Some content</div>
+        </CardTab>
+        <CardTab value="two" label="Option 2">
+          <div style={cardContentStyle}>Some content 2</div>
+        </CardTab>
+        <CardTab value="three" label="Option 3">
+          <div style={cardContentStyle}>Some content 3</div>
+        </CardTab>
+        <CardTab value="four" label="Option 4">
+          <div style={cardContentStyle}>Some content 4</div>
+        </CardTab>
+      </TabbedCard>
+    </Wrapper>;
+}`,...(L=(R=p.parameters)==null?void 0:R.docs)==null?void 0:L.source}}};const gt=["Default","WithFooterLink","WithControlledTabValue"];export{s as Default,p as WithControlledTabValue,m as WithFooterLink,gt as __namedExportsOrder,Ct as default};
---
+++ storybook/assets/Toolkit.stories-DAaNho4a.js
@@ -0,0 +1,30 @@
+import{R as o}from"./index-CV65Cz2T.js";import{c as e}from"./plugin-O_3GFsBU.js";import{S as u}from"./Grid-BB-GYNIy.js";import{C as p}from"./ComponentAccordion-DnvV5kmu.js";import{w as g}from"./appWrappers-CKD5lEjk.js";import{T as i}from"./TemplateBackstageLogoIcon-EPOdXbBm.js";import{I as k}from"./InfoCard-Mery5rmd.js";import"./iframe-Be_b3fxo.js";import"./Plugin-DiuNzj8P.js";import"./componentData-Ch1YzSDK.js";import"./ApiRef-DSSgzaW4.js";import"./useAnalytics-BvU8z2Qw.js";import"./ConfigApi-BmcvyZGI.js";import"./index-BuiljCIz.js";import"./useRouteRef-DpQDGLPk.js";import"./interopRequireDefault-Y9pwbXtE.js";import"./createSvgIcon-esOf8MXJ.js";import"./capitalize-BKQXumqD.js";import"./defaultTheme-dorO8Ycy.js";import"./withStyles-5L4Wrt0H.js";import"./hoist-non-react-statics.cjs-DscmM33_.js";import"./createChainedFunction-Da-WpsAN.js";import"./createSvgIcon-BE9FbSc8.js";import"./debounce-DtXjJkxj.js";import"./isMuiElement-DG475NJP.js";import"./ownerWindow-D1FXuRdm.js";import"./useIsFocusVisible-DMiTmV9E.js";import"./index-CpD63NMF.js";import"./useControlled-ltvK4nzt.js";import"./unstable_useId-DsWtIzNz.js";import"./IconButton-CiUvO6Eb.js";import"./ButtonBase-Ba4BxsIu.js";import"./TransitionGroupContext-C1n1zRT4.js";import"./DialogTitle-CfPI47rC.js";import"./Modal-CgSagCB9.js";import"./classCallCheck-BNzALLS0.js";import"./Portal-Cz7V7drg.js";import"./Backdrop-CckK7b0s.js";import"./useTheme-Dmunefpm.js";import"./utils-CaJHH84X.js";import"./Paper-BZMBdJdO.js";import"./Typography-ClGfHewG.js";import"./Button-Bud8S-Ub.js";import"./WebStorage-DIH418uN.js";import"./index-DWA0-5lI.js";import"./MockTranslationApi-cJ1q0vu7.js";import"./inherits-CeNoR6ST.js";import"./toArray-COxZ3wB-.js";import"./TranslationApi-BZ5LsqDR.js";import"./ExpandMore-CGIqGUc-.js";import"./makeStyles-B2ffWluB.js";import"./AccordionDetails-Pkymkb_v.js";import"./react-is.production.min-D0tnNtx9.js";import"./Collapse-CydqjKxS.js";import"./useAsync-cwiHyrW_.js";import"./useMountedState-BG_s6PIB.js";import"./isSymbol-B6IIivl5.js";import"./isObject-CDdS0vB1.js";import"./toString-C4GbLIMt.js";import"./ApiProvider-BkbfiZHS.js";import"./index-BDlCPcVq.js";import"./CssBaseline-Lx8PNlwq.js";import"./ThemeProvider-Cal7rCVq.js";import"./jsx-runtime-B1AhRb-V.js";import"./palettes-C1DVN5xY.js";import"./index-DSEhRz7_.js";import"./CardActions-BQPFvyux.js";import"./CardContent-B5iYBdc5.js";import"./ErrorBoundary-BXVjW1bX.js";import"./translation-CnlFI5r0.js";import"./ErrorPanel-CtwI3OOl.js";import"./WarningPanel-CS4JIXKB.js";import"./MarkdownContent-C0_8Uxj-.js";import"./CodeSnippet-D0EJogpP.js";import"./objectWithoutProperties-Et-85jJS.js";import"./toConsumableArray-BEwiObev.js";import"./Box-D_ilMR00.js";import"./typography-Cs1aOEfH.js";import"./CopyTextButton-CWv1yKFy.js";import"./useCopyToClipboard-CXmmh4yb.js";import"./Tooltip-Dky-G2Xk.js";import"./Popper-CtVHcIVU.js";import"./Grow-JHyMhSnP.js";import"./List-D3T44CW6.js";import"./ListContext-DDd8B5Hf.js";import"./ListItem--gnxU6mZ.js";import"./ListItemText-Db7RxVPo.js";import"./LinkButton-PUAgVPlG.js";import"./Link-DPwPNjg6.js";import"./lodash-D2SO6J9k.js";import"./CardHeader-DAExE-wZ.js";import"./Divider-YBHdkOJo.js";import"./BottomLink-CQ5BwGoW.js";import"./ArrowForward-IZFOs--K.js";const ir={title:"Plugins/Home/Components/Toolkit",decorators:[m=>g(o.createElement(m,null))]},r=()=>o.createElement(u,{item:!0,xs:12,md:6},o.createElement(e,{tools:Array(8).fill({url:"#",label:"link",icon:o.createElement(i,null)})})),t=()=>{const m=f=>o.createElement(p,{expanded:!0,...f});return o.createElement(k,{title:"Toolkit",noPadding:!0},o.createElement(u,{item:!0},o.createElement(e,{title:"Tools 1",tools:Array(8).fill({url:"#",label:"link",icon:o.createElement(i,null)}),Renderer:m}),o.createElement(e,{title:"Tools 2",tools:Array(8).fill({url:"#",label:"link",icon:o.createElement(i,null)}),Renderer:p}),o.createElement(e,{title:"Tools 3",tools:Array(8).fill({url:"#",label:"link",icon:o.createElement(i,null)}),Renderer:p})))};r.__docgenInfo={description:"",methods:[],displayName:"Default"};t.__docgenInfo={description:"",methods:[],displayName:"InAccordian"};var l,n,a;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`() => {
+  return <Grid item xs={12} md={6}>
+      <HomePageToolkit tools={Array(8).fill({
+      url: '#',
+      label: 'link',
+      icon: <TemplateBackstageLogoIcon />
+    })} />
+    </Grid>;
+}`,...(a=(n=r.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var c,s,d;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`() => {
+  const ExpandedComponentAccordion = (props: any) => <ComponentAccordion expanded {...props} />;
+  return <InfoCard title="Toolkit" noPadding>
+      <Grid item>
+        <HomePageToolkit title="Tools 1" tools={Array(8).fill({
+        url: '#',
+        label: 'link',
+        icon: <TemplateBackstageLogoIcon />
+      })} Renderer={ExpandedComponentAccordion} />
+        <HomePageToolkit title="Tools 2" tools={Array(8).fill({
+        url: '#',
+        label: 'link',
+        icon: <TemplateBackstageLogoIcon />
+      })} Renderer={ComponentAccordion} />
+        <HomePageToolkit title="Tools 3" tools={Array(8).fill({
+        url: '#',
+        label: 'link',
+        icon: <TemplateBackstageLogoIcon />
+      })} Renderer={ComponentAccordion} />
+      </Grid>
+    </InfoCard>;
+}`,...(d=(s=t.parameters)==null?void 0:s.docs)==null?void 0:d.source}}};const mr=["Default","InAccordian"];export{r as Default,t as InAccordian,mr as __namedExportsOrder,ir as default};
